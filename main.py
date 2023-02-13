15f83482f37501e4d2806243a947e6f6dbeb4b68
Atudo <jimpirate49@gmail.com>
2023-02-13 16:06:48 +0300

    extend the program by introducing security feature for the cards and implementing inheritance

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas
 
 df = pandas.read_csv("hotels.csv", dtype={"id": str})
 df_cards = pandas.read_csv("cards.csv", dtype=str).to_dict(orient="records")
+df_security_cards = pandas.read_csv("card_security.csv", dtype=str)
 
 
 class Hotel:
@@ -19,8 +20,6 @@ class Hotel:
         availability = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
         if availability == "yes":
             return True
-        else:
-            return False
 
 
 class ReservationTicket:
@@ -49,18 +48,28 @@ class CreditCard:
             return True
 
 
+class SecureCreditCard(CreditCard):
+    def authenticate(self, given_password):
+        password = df_security_cards.loc[df_security_cards["number"] == self.number, "password"].squeeze()
+        if password == given_password:
+            return True
+
+
 print(df)
 hotel_ID = input("Enter the id of the hotel: ")
 hotel = Hotel(hotel_ID)
 
 if hotel.available():
-    credit_card = CreditCard(number="1234")
+    credit_card = SecureCreditCard(number="1234567890123456")
     if credit_card.validate(expiration="12/26",
                             holder="JOHN SMITH", cvc="123"):
-        hotel.book()
-        name = input("Enter your name: ")
-        reservation_ticket = ReservationTicket(cust_name=name, hotel_object=hotel)
-        print(reservation_ticket.generate())
+        if credit_card.authenticate(given_password="mypass"):
+            hotel.book()
+            name = input("Enter your name: ")
+            reservation_ticket = ReservationTicket(cust_name=name, hotel_object=hotel)
+            print(reservation_ticket.generate())
+        else:
+            print("Credit card authentication failed")
     else:
         print("Invalid card details")
 else:
