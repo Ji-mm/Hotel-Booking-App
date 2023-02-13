9335f3f69741deb9a469ca1734785a34ac389b1b
Atudo <jimpirate49@gmail.com>
2023-02-13 14:59:52 +0300

    extend the program by implementing a card feature

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas
 
 df = pandas.read_csv("hotels.csv", dtype={"id": str})
+df_cards = pandas.read_csv("cards.csv", dtype=str).to_dict(orient="records")
 
 
 class Hotel:
@@ -37,13 +38,30 @@ class ReservationTicket:
         return content
 
 
+class CreditCard:
+    def __init__(self, number):
+        self.number = number
+
+    def validate(self, expiration, holder, cvc):
+        card_data = {"number": self.number, "expiration": expiration,
+                     "holder": holder, "cvc": cvc}
+        if card_data in df_cards:
+            return True
+
+
 print(df)
 hotel_ID = input("Enter the id of the hotel: ")
 hotel = Hotel(hotel_ID)
+
 if hotel.available():
-    hotel.book()
-    name = input("Enter your name: ")
-    reservation_ticket = ReservationTicket(cust_name=name, hotel_object=hotel)
-    print(reservation_ticket.generate())
+    credit_card = CreditCard(number="1234")
+    if credit_card.validate(expiration="12/26",
+                            holder="JOHN SMITH", cvc="123"):
+        hotel.book()
+        name = input("Enter your name: ")
+        reservation_ticket = ReservationTicket(cust_name=name, hotel_object=hotel)
+        print(reservation_ticket.generate())
+    else:
+        print("Invalid card details")
 else:
     print("Sorry. Hotel currently unavailable")
